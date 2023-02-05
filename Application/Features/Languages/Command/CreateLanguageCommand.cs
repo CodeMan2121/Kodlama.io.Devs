c51c1aee016aad70e307c107c7a4feeee16a4815
Administrator <Administrator@KINGHAZE>
2023-02-05 12:41:36 +0300

    Add project files.

---
+++ Application/Features/Languages/Command/CreateLanguageCommand.cs
@@ -0,0 +1,38 @@
+﻿using Application.Features.Languages.Dtos;
+using Application.IRepository;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Languages.Command
+{
+    public class CreateLanguageCommand : IRequest<ILanguageDto>
+    {
+        public class CreateLanguageCommandHandle : IRequestHandler<CreateLanguageCommand, ILanguageDto>
+        {
+            private readonly ILanguageRepository _languageRepositor;
+            private readonly IMapper _mapper;
+
+            public CreateLanguageCommandHandle(ILanguageRepository languageRepository, IMapper mapper)
+            {
+                _languageRepositor = languageRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<ILanguageDto> Handle(CreateLanguageCommand request, CancellationToken cancellationToken)
+            {
+                Language mappedlanguage = _mapper.Map<Language>(request);
+                Language createdLanguage = await _languageRepositor.AddAsync(mappedlanguage);
+                ILanguageDto createdLanguageDto = _mapper.Map<ILanguageDto>(createdLanguage);
+                return createdLanguageDto;
+            }
+
+        }
+    }
+}
---
+++ Persistence/Repositories/LanguageRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.IRepository;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories
+{
+    public class LanguageRepository : EfRepositoryBase<Language, BaseDbContext>, ILanguageRepository
+    {
+    }
+}
